3f90825a3c3bead28fd1a229a6ffcfd28fdc162a
winrid <winrid@gmail.com>
2020-04-12 15:31:05 -0700

    nodejs SSO example

---
+++ sso/nodejs/README.md
@@ -0,0 +1,6 @@
+This is a demo of FastComments SSO in a very simple ExpressJS app.
+
+See routes/index.js for how to create the SSO payload **server-side**.
+See views/index.ejs for how to pass the SSO payload to FastComments **client-side**.
+
+To run, run "npm install && npm run start" and open http://localhost:3003
---
+++ sso/nodejs/routes/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const crypto = require('crypto');
+
+const API_SECRET = 'DEMO_API_SECRET'; // Replace this with your own in production! This will only work with the "demo" tenant id.
+
+router.get('/', function (req, res, next) {
+    // Create the user object from your database.
+    const someUser = {
+        email: 'someguy@someplace.com',
+        username: 'some-username',
+        avatar: 'https://static.fastcomments.com/1582299581264-69384190_3015192525174365_476457575596949504_o.jpg'
+    };
+
+    const userDataJSON = JSON.stringify(someUser);
+    const userDataJSONBase64 = Buffer.from(userDataJSON).toString('base64');
+
+    const now = Date.now();
+    const verificationHash = crypto
+        .createHmac('sha256', API_SECRET)
+        .update(now + userDataJSONBase64)
+        .digest('hex');
+
+    const ssoConfig = {
+        userDataJSONBase64: req.query.logout === 'true' ? null : userDataJSONBase64, // implement your own login/out mechanism.
+        verificationHash: req.query.logout === 'true' ? null : verificationHash,
+        timestamp: now,
+        logoutURL: '/?logout=true',
+        loginURL: '/'
+    };
+
+    res.render('index.ejs', {
+        title: 'FastComments SSO Demo',
+        ssoConfig: ssoConfig,
+        urlId: '/' // We only hard code the url-id since our logout url has query params that would change the page URL. You probably don't need to do this.
+    });
+});
+
+module.exports = router;
