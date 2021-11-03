0256b4042c9a4b16ebc307b31f3a33d677cc5cb9
winrid <winrid@gmail.com>
2021-11-02 17:13:51 -0700

    adding vue example (to test esm packaging works...)

---
+++ spa/vuejs3/live-chat/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+    <fast-comments-live-chat v-bind:config="{tenantId: 'demo'}"/>
+</template>
+
+<script>
+import {FastCommentsLiveChat} from 'fastcomments-vue-next';
+
+export default {
+  name: 'App',
+  components: {
+    FastCommentsLiveChat
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
