fbc86103e0d36019364ee96654d2ee1f7b6f6507
winrid <winrid@gmail.com>
2022-08-25 17:28:58 -0700

    adding sso api example + making some constants easier to identify

--- sso/nodejs/README.md
+++ sso/nodejs/README.md
@@ -4,5 +4,6 @@ See routes/index.js for how to create the SSO payload **server-side**.
 See views/index.ejs for how to pass the SSO payload to FastComments **client-side**.
 
 This will work as-is since it uses a pre-created demo account, however in production you'd have to define your own secret key and tenant id.
+You may set your own api key and tenant id in `routes/index.js`.
 
 To run, run "npm install && npm run start" and open http://localhost:3003
--- sso/nodejs/routes/index.js
+++ sso/nodejs/routes/index.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const crypto = require('crypto');
 
 const API_SECRET = 'DEMO_API_SECRET'; // Replace this with your own in production! This will only work with the "demo" tenant id.
+const TENANT_ID = 'demo'; // REPLACE THIS WITH YOUR TENANT ID
 
-router.get('/', function (req, res, next) {
+function getSSOConfig(req) {
     // Create the user object from your database.
     const someUser = {
-        id: 'some-user-id',
-        email: 'someone@someplace.com',
-        username: 'some-username',
+        id: 1,
+        email: 'someone@somewhere.com',
+        username: 'someone',
         displayName: 'Some Username',
         avatar: 'https://static.fastcomments.com/1582299581264-69384190_3015192525174365_476457575596949504_o.jpg',
         optedInNotifications: true,
@@ -28,19 +29,35 @@ router.get('/', function (req, res, next) {
         .update(now + userDataJSONBase64)
         .digest('hex');
 
-    const ssoConfig = {
+    return {
         userDataJSONBase64: req.query.logout === 'true' ? null : userDataJSONBase64, // implement your own login/out mechanism.
         verificationHash: req.query.logout === 'true' ? null : verificationHash,
         timestamp: now,
         logoutURL: '/?logout=true',
         loginURL: '/'
     };
+}
 
+// server-side rendering example
+router.get('/', function (req, res, next) {
     res.render('index.ejs', {
+        TENANT_ID,
         title: 'FastComments SSO Demo',
-        ssoConfig: ssoConfig,
-        urlId: '/' // We only hard code the url-id since our logout url has query params that would change the page URL. You probably don't need to do this.
+        ssoConfig: getSSOConfig(req),
+        urlId: 'some-page-id-or-url' // We only hard code the url-id since our logout url has query params that would change the page URL. You probably don't need to do this.
     });
+});
+
+// REST API example
+router.get('/sso-user-info', function (req, res, next) {
+    res.set('Access-Control-Allow-Origin', req.headers['origin']);
+    res.set('Access-Control-Allow-Credentials', 'true');
+    res.send(getSSOConfig(req));
+});
+router.options('/sso-user-info', function (req, res, next) {
+    res.set('Access-Control-Allow-Origin', req.headers['origin']);
+    res.set('Access-Control-Allow-Credentials', 'true');
+    res.status(200).end();
 });
 
 module.exports = router;
